a14c9241574c1a36fb5da79d41ca8f4136d2d22d
Jakob Hariz <jh@Jakobs-MacBook-Pro.local>
2014-06-29 19:16:47 +0200

    Fixed forward compat flag in glfwWindowHint in mainwindow.cpp to make it run on osx

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -45,11 +45,12 @@ bool MainWindow::initGLFW()
 			return false;
 		}
 
-		glfwWindowHint(GLFW_SAMPLES, 4);
+		//glfwWindowHint(GLFW_SAMPLES, 4);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	
-
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Needed for osx
+		
 		INITIALIZED = true;
 	}
 
